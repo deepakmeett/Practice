61197e77ea3d65acd497730d9260bce10963fca6
Deepak <deepak@dumpin.in>
2021-04-07 21:01:57 +0530

    Interview Package created reverseAnArray done.

---
+++ app/src/main/java/com/example/practice/interview/reverseAnArray.java
@@ -0,0 +1,13 @@
+package com.example.practice.interview;
+public class reverseAnArray {
+
+    public static void main(String[] args) {
+        
+        int[] integerArray = {4, 5, 1, 2};
+        
+        for (int i = 0; i < integerArray.length; i++) {
+            int index = (integerArray.length)-i;
+            System.out.print(integerArray[index]+", ");
+        }
+    }
+}
